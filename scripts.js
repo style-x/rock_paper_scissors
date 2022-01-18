9afffffccbc141936dd7392556e963dbc003d4a9
xan <47989552+style-x@users.noreply.github.com>
2022-01-18 20:04:03 +0100

    Konsolen Ausgabe stimmt
    Durch drei Buttons kann der Spieler eine Eingabe machen und diese wird der zufallsgenerierten Ausgabe in der Konsole zusammen ausgegeben.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
 
 function game() {
 
-  for (i = 0; i < 5; i++) {
+  let Array = ['Rock', 'Paper', 'Scissors'];
+  let playerSelection;
+  let computerSelection;
 
-    function playRound(playerSelection, computerSelection) {
-      
-    }
+  function computerPlay() {
+    let rand = Math.floor(Math.random() * Array.length);
+    return Array[rand];
+  };
 
-    function computerPlay() {
-      let Array = ['Rock', 'Paper', 'Scissors'];
-      let rand = Math.floor(Math.random() * Array.length);
-
-      console.log(Array[rand]);
-    }
-    
-    const playerSelection = window.prompt('Rock-Paper-Scissors- Your Choice:'); 
-    const computerSelection = computerPlay();
-
-    console.log(playerSelection);
-    //console.log(playRound(playerSelection, computerSelection));
-  }
-
-}
+  const PlayerClick = document.querySelectorAll('.btn').forEach(item => {
+    item.addEventListener('click', event => {
+      playerSelection = event.target.innerText;
+      computerSelection = computerPlay();
+      playRound(playerSelection, computerSelection);
+    })
+  });
+  
+  function playRound(playerSelection, computerSelection) {
+    console.log(`${playerSelection} vs ${computerSelection}`)
+  };
+ 
+};
 
 game();

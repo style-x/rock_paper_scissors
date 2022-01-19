2cb7becead352e513156fb1475cf7ec9827facca
xan <47989552+style-x@users.noreply.github.com>
2022-01-19 20:21:43 +0100

    Punkte zählen
    Die Punkte werden nun richtig gezählt, doch leider noch nur in der Konsole.

--- scripts.js
+++ scripts.js
@@ -2,25 +2,56 @@
 function game() {
 
   let Array = ['Rock', 'Paper', 'Scissors'];
-  let playerSelection;
-  let computerSelection;
+  let ps;
+  let cs;
+  let pScore = 0;
+  let cScore = 0;
 
   function computerPlay() {
     let rand = Math.floor(Math.random() * Array.length);
     return Array[rand];
   };
 
+  let Score = document.getElementById('scoreboard').innerText;
+
   const PlayerClick = document.querySelectorAll('.btn').forEach(item => {
     item.addEventListener('click', event => {
-      playerSelection = event.target.innerText;
-      computerSelection = computerPlay();
-      playRound(playerSelection, computerSelection);
+      ps = event.target.innerText;
+      cs = computerPlay();
+      playRound(ps, cs);
     })
   });
   
-  function playRound(playerSelection, computerSelection) {
-    console.log(`${playerSelection} vs ${computerSelection}`)
-  };
+  function playRound(ps, cs) {
+
+      if (ps == cs) {
+        console.log("draw");
+      } else if (ps == "Rock" && cs !== "Paper") {
+        console.log(`${ps}:${cs} Player gewinnt`);
+        pScore++;
+      } else if (ps == "Paper" && cs !== "Scissors") {
+        console.log(`${ps}:${cs} Player gewinnt`);
+        pScore++;
+      } else if (ps == "Scissors" && cs !== "Rock") {
+        console.log(`${ps}:${cs} Player gewinnt`);
+        pScore++;
+      } else if (cs == "Rock" && ps !== "Paper") {
+        console.log(`${ps}:${cs} PC gewinnt`);
+        cScore++;
+      } else if (cs == "Paper" && ps !== "Scissors") {
+        console.log(`${ps}:${cs} PC gewinnt`);
+        cScore++;
+      } else if (cs == "Scissors" && ps !== "Rock") {
+        console.log(`${ps}:${cs} PC gewinnt`);
+        cScore++;
+      } else {
+        console.log("error?!");
+      };
+
+      console.log(`${pScore} ${cScore}`);
+      Score = "Test!";
+
+    };
  
 };
 
